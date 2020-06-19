da08f21b6cabc335c81c6e4dc099265049c1aa49
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-19 19:08:21 +0530

    added some functions

--- app.js
+++ app.js
@@ -11,6 +11,41 @@ checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("John", 17);
 
+// decalring an array
+var vegetables = ["cauliflowers", "okras", "tomatoes", "potatoes", "beans"];
+for(var i = 0; i < vegetables.length; i++) {
+    console.log(vegetables[i]);
+}
+
+let pet = {
+    name: 'John Doe',
+    breed: 'muley'
+}
+
+var people = [
+    {name: "ShadowWalker", age: 21},
+    {name: "NoWay", age: 18},
+    {name: "Muley", age: 23},
+    {name: "Unseen", age: 20},
+    {name: "Naratuga", age: 15},
+]
+
+for(var i = 0; i < people.length; i++ ){
+    console.log(people[i].name);
+    console.log(people[i].age);
+    checkAge(people[i].name, people[i].age);
+}
+
+console.log(Object.assign(pet, {
+    name: "Marley",
+    breed: "Golden Retreiver"
+}))
+
+pet.name = "Buster"
+pet.breed = "German Shepherd"
+
+
+
 
 
 
